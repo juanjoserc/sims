ac7354933b2f680cd1fa081e83581e388b08a1fa
juanjoserc <juanjoserc@gmail.com>
2015-09-05 14:47:27 -0500

    Gem file updated for Heroku 4

--- db/migrate/20150831133828_add_tech_id_to_ticket.rb
+++ db/migrate/20150831133828_add_tech_id_to_ticket.rb
@@ -1,6 +1,6 @@
 class AddTechIdToTicket < ActiveRecord::Migration
   def change
     add_reference :tickets, :tech, index: true
-    add_foreign_key :tickets, :teches
+    add_foreign_key :tickets, :users
   end
 end
