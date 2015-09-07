1ab0f6733ecd1d32141cd1fc71ce58b917689e0d
juanjoserc <juanjoserc@gmail.com>
2015-09-07 08:17:54 -0500

    Added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+TicketStatus.destroy_all
+Service.destroy_all
+TicketStatus.create!(name: 'Pending Tech Assignment')
+TicketStatus.create!(name: 'Open')
+TicketStatus.create!(name: 'Closed')
+Service.create!([{
+		name: 'Armory Services',
+		description: 'Provides all the weaponry and ammunition needed for a mission.'
+	},
+	{
+		name:'Transportation Services',
+		description:'Provides vehicles and transportation services during a mission.'
+	},
+	{
+		name:'Special Devises Services',
+		description:'You know, from phone shoes to c4 bubble gum.'
+	}
+
+])
+
+10.times do |i|
+  Service.create(name: "Service id:#{i}", description: "Sample Service #{i}")
+end
+
+
+p "Ticket status created: #{TicketStatus.count}. Services created: #{Service.count}"
